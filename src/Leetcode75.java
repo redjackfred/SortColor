c2b1ac1da779e269c02d60ab2bca0540b193961b
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-02-03 19:24:12 -0800

    Initial commit

--- src/Leetcode75.java
+++ src/Leetcode75.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Leetcode75 {
+public class Leetcode75 {
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for(int i = 0; i < nums.length; i++){
+            count[nums[i]]++;
+        }
+        int i = 0, j = 0;
+        while(j < nums.length){
+            if(count[i] == 0){
+                i++;
+            }else{
+                nums[j++] = i;
+                count[i]--;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Leetcode75 leetcode75 = new Leetcode75();
+        int[] nums = {2,0,2,1,1,0};
+        leetcode75.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
